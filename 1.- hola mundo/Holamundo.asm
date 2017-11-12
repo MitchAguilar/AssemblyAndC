725dbf44581b3e06228850c67b3b59160971849a
MitchAguilar <ma.aguilar@udla.edu.co>
2017-11-12 01:59:51 -0500

    firt hello world en asm

--- 1.- hola mundo/Holamundo.asm
+++ 1.- hola mundo/Holamundo.asm
@@ -1,6 +1,6 @@
 section .data
 
-msg db "hola mundo!!!",0xA,xD
+msg db "hola mundo!!!",0xA,0xD
 len equ $ - msg
 
 section .text
@@ -10,7 +10,7 @@ _start:
     mov eax, 4 ; llamda al sistema(sys_ write)
     ;eax= 4
     mov ebx, 1 ;stout
-    mov esx, msg ;msm _salid
+    mov ecx, msg ;msm _salid
     mov edx, len ; longitud msg 
 
     int 0x80 ; interruccion, llamada al sistema de interruciones
