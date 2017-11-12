86670ebb14785560f304218b84fb193869d643e2
MitchAguilar <ma.aguilar@udla.edu.co>
2017-11-12 03:16:37 -0500

    end second part

---
+++ 2.-AutomatizandoElEnsamblado/Holamundo.asm
@@ -0,0 +1,20 @@
+section .data
+
+msg db "hola mundo!!!",0xA,0xD
+len equ $ - msg
+
+section .text
+    global _start
+_start:
+    
+    mov eax, 4 ; llamda al sistema(sys_ write)
+    ;eax= 4
+    mov ebx, 1 ;stout
+    mov ecx, msg ;msm _salid
+    mov edx, len ; longitud msg 
+
+    int 0x80 ; interruccion, llamada al sistema de interruciones
+    mov eax, 1 ;exit
+    int 0x80 ; interruccion invocada
+
+
